f600aa6e15eb928180db736b9c09335fe3065c5f
watership <silentzhou@gmail.com>
2024-12-18 00:48:23 +0800

    initialization

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GLM-4V-Flash-Case
+
+
+- 智谱AI开放模型 GLM-4V-Flash 的API的微信小程序的演示代码。
+
+- 相关演示视频：[《免费且强大！GLM-4V-Flash多模态模型，轻松打造专属AI应用！》](https://www.bilibili.com/video/BV1kHBTYxEKx/)。 
---
+++ miniprogram/pages/user-center/index.js
@@ -0,0 +1,160 @@
+const defaultImg_path = "../../images/20241212185556.jpg"
+Page({
+  data: {
+    img_path:defaultImg_path,
+    img_base64:'',
+    message:'',
+    photoDisabled:false,
+    cookbookDisabled:false
+  },
+  onLoad(){
+    
+  },
+  copyCode(e) {
+    const code = e.target?.dataset?.code || '';
+    wx.setClipboardData({
+      data: code,
+      success: () => {
+        wx.showToast({
+          title: '已复制',
+        })
+      },
+      fail: (err) => {
+        console.error('复制失败-----', err);
+      }
+    })
+  },
+
+  discoverCloud() {
+    wx.switchTab({
+      url: '/pages/examples/index',
+    })
+  },
+
+  gotoGoodsListPage() {
+    wx.navigateTo({
+      url: '/pages/goods-list/index',
+    })
+  },
+  // 转换为Base64
+  convertToBase64: function (filePath) {
+    const fs = wx.getFileSystemManager();
+
+    fs.readFile({
+        filePath: filePath,
+        encoding: 'base64', // 转换为 Base64
+        success: (res) => {
+            const imgBase64 = `data:image/jpeg;base64,${res.data}`;
+            this.uploadToServer(imgBase64);
+        },
+        fail: (error) => {
+            console.error(error);
+        }
+    });
+},
+  takePhoto(){
+    let me = this;
+    wx.chooseMedia({
+      count: 1, // 默认9
+      mediaType:['image'],
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (result) {
+        let imgURL = result.tempFiles[0].tempFilePath;
+        let imgName = `${new Date().getTime()}${imgURL.substring(imgURL.lastIndexOf("."))}`
+        wx.showLoading({
+          title: '上传中',
+        })
+        wx.cloud.uploadFile({
+          cloudPath: imgName, // 上传至云端的路径
+          filePath: imgURL, // 小程序临时文件路径
+          success: res => {
+            // 返回文件 ID
+            let fileId = res.fileID
+            console.log(fileId)
+            wx.cloud.getTempFileURL({
+              fileList: [fileId],
+              success: res => {
+                wx.hideLoading();
+                let imgurl = res.fileList[0].tempFileURL;
+                console.log("获取url地址：",imgurl);
+                me.setData({
+                  'img_path':imgurl,
+                  'message':''
+                })
+              },
+              fail: console.error
+            })
+          },
+          fail: console.error
+        })
+      },
+      fail: function (res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+  takeCookbook(){
+    this.requestAI()
+  },
+  requestAI(){	
+    // 初始化 API Key 和请求参数
+  const apiKey = "xxxxxx"; // 替换为您的真实 API 密钥
+  const apiUrl = "https://open.bigmodel.cn/api/paas/v4/chat/completions"; // API 请求地址
+
+  const requestData = {
+      model: "glm-4v-flash", // 指定模型名称
+      messages: [
+          {
+              role: "user",
+              content: [
+                {
+                  "type": "text",
+                  "text": "请分析这幅绘画作品，识别画作的主题、表达内容和风格，并提供创作改进建议,注意建议使用鼓励的语气和话术。"
+                },
+                {
+                  "type": "image_url",
+                  "image_url": {
+                      "url" : this.data.img_path
+                  }
+                }
+              ]
+          }
+      ]
+  };
+    let me = this;
+    wx.showLoading({
+      title: '获取中',
+    })
+    wx.request({
+      url: apiUrl, //仅为示例，并非真实的接口地址
+      method:'POST',
+      data: requestData
+     ,
+      header: {
+        'content-type': 'application/json', // 默认值
+        'Authorization': `Bearer ${apiKey}`
+      },
+      success (res) {
+        wx.hideLoading()
+        console.log(res.data)
+        me.setData({
+          message: res.data.choices[0].message.content.replace(/\\n/g, '\n'),
+        })
+      }
+    })
+
+
+    const ctx  = wx.createCameraContext()
+    ctx.takePhoto({
+      quality:"high", //高质量的图片
+      success: res => {
+        //res.tempImagePath照片文件在手机内的的临时路径
+        let tempImagePath = res.tempImagePath
+        this.setData({
+          src: tempImagePath
+        })
+      }
+    })
+  },
+});
